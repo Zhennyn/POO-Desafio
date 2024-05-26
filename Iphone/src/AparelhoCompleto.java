4ac3170d2f501b8a1fc21c5c9c3541ad18a507f5
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-05-26 11:49:20 -0300

    Teste

---
+++ Iphone/src/AparelhoCompleto.java
@@ -0,0 +1,11 @@
+public class AparelhoCompleto {
+    public static void main(String[] args) {
+        Celular celular = new Celular();
+
+        AparelhoTelefonico.celular();
+        NavegadorInternet.celular();
+        ReprodutorMusical.celular();
+
+
+    }    
+}
